d759afd4c05e8d611bde9795be95eff6998ef7c3
Grimiku <kanciok01@yahoo.pl>
2026-03-23 10:26:57 +0100

    Maybe fix HB

--- renpy/text/hbfont.pyx
+++ renpy/text/hbfont.pyx
@@ -21,8 +21,8 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from __future__ import print_function
-
 from sdl2 cimport *
+from libc.string cimport memcpy
 from pygame_sdl2 cimport *
 import_pygame_sdl2()
 
@@ -247,19 +247,15 @@ cdef bint is_zerowidth(unsigned int char):
     return False
 
 cdef unsigned long io_func(FT_Stream stream, unsigned long offset, unsigned char *buffer, unsigned long count):
-    """
-    Seeks to offset, and then reads count bytes from the stream into buffer.
-    """
-
     cdef HBFace face
     cdef char *cbuf
     cdef unsigned long i
+    cdef const unsigned char *cbuf2  # add this
 
     face = <HBFace> stream.descriptor.pointer
     f = face.f
 
     if face.offset != offset:
-
         try:
             f.seek(offset)
             face.offset = offset
@@ -270,17 +266,14 @@ cdef unsigned long io_func(FT_Stream stream, unsigned long offset, unsigned char
     if count != 0:
         try:
             buf = f.read(count)
-            cbuf = buf
             count = len(buf)
-
-            for i from 0 <= i < count:
-                buffer[i] = cbuf[i]
+            cbuf2 = buf
+            memcpy(buffer, cbuf2, count)
         except Exception:
             traceback.print_exc()
             return -1
 
     face.offset += count
-
     return count
 
 cdef void close_func(FT_Stream stream):
